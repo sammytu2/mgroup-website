a4e54f2f6ab8b9387346bbdbd5c114899f7c6593
Sam Tu <samtu2@gmail.com>
2018-06-06 02:41:26 -0700

    hover over keymembers will show text

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+function gatherKeyInfo(){
+
+    x[0].innerHTML = "Hello World!";
+};
+function clickMembers(keyMemberNumber){
+var x = document.getElementsByClassName("keyInfo");
+    x[keyMemberNumber].style.visibility="visible";
+};
+function mouseOutMembers(keyMemberNumber){
+var x = document.getElementsByClassName("keyInfo");
+     x[keyMemberNumber].style.visibility="hidden";
+};
